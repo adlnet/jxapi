a7c48095480e52bbfa2884bc48fcb8e4f76834f0
Louis Wolford <lou.wolford.ctr@adlnet.gov>
2015-11-13 14:37:12 -0500

    added checkpath comment

--- src/main/java/gov/adlnet/xapi/client/BaseClient.java
+++ src/main/java/gov/adlnet/xapi/client/BaseClient.java
@@ -256,6 +256,7 @@ public class BaseClient {
         }
     }
 
+    // When retrieving 'more' statements, LRS will return full path..the client will have part in the URI already so cut that off
     protected String checkPath(String path){
         if (path.toLowerCase().contains(this._host.getPath().toLowerCase())){
             int pathLength = this._host.getPath().length();
